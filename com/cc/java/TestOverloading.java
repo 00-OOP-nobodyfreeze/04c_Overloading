e1a4d9d60d524eee1354fd505b1aaf9adbaf9d19
nobodyfreeze <office.fherzog@gmail.com>
2023-07-31 12:16:57 +0200

    activ example

--- com/cc/java/TestOverloading.java
+++ com/cc/java/TestOverloading.java
@@ -28,6 +28,7 @@ public class TestOverloading {
 	
 	
 	// Gleiche Signatur 3 | Access Modifier ?
+
 	// public void sayHello() {
 	// 	System.out.println("Hello");
 	// }
@@ -39,6 +40,7 @@ public class TestOverloading {
 	
 		
 	// Gleiche Signatur 3 | Parameter ?
+
 	// public void sayHello2(String name, int counter) {
 	// 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
 	// }
@@ -49,6 +51,7 @@ public class TestOverloading {
 
 
 	// Gleiche Signatur 3 | Parameter Namen ?
+
 	// public void sayHello2(String name, int counter) {
 	// 	System.out.println("Parameter name: " + name + " | Parameter: counter: " + counter);
 	// }
@@ -63,28 +66,26 @@ public class TestOverloading {
 		System.out.println("1: Parameter name: " + name + " | Parameter: counter: " + counter);
 	}
 	
-	// // Überladene Methode 1 (Reihenfolge Parameter)
+	// Überladene Methode 1 (Reihenfolge Parameter)
 	public void sayHello2(int counter, String name) {
 		System.out.println("2: Parameter name: " + name + " | Parameter: counter: " + counter);
 	}
 	
-	// // Überladene Methode 3 (Anzahl Parameter)
+	// Überladene Methode 3 (Anzahl Parameter)
 	public void sayHello2() {
 		System.out.println("3: Ohne parameter");
 	}
 	
 	
-	// // BasisMethode
+	// BasisMethode
 	public void sayHello3(String name, int ID) {
 		System.out.println("Parameter name: " + name + " | Parameter: ID: " + ID);
 	}
 	
 	
-	// // Überladene Methode 3 (Parameter Typ)
+	// Überladene Methode 3 (Parameter Typ)
 	public void sayHello3(String name, String ID) {
 		System.out.println("Parameter name: " + name + " | Parameter: ID: " + ID);
 	}
 	
-
-
 }
